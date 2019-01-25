b011505d83c5d6997078c5ccf1f140a8a034fc56
Brock <bcraft@uw.edu>
2019-01-24 17:27:36 -0800

    First draft

--- _440_MKR1000_Tempserver.ino
+++ _440_MKR1000_Tempserver.ino
@@ -1,31 +1,43 @@
 /*
-  WiFi Web Server LED Blink
 
- A simple web server that lets you blink an LED via the web.
- This sketch will print the IP address of your WiFi Shield (once connected)
- to the Serial monitor. From there, you can open that address in a web browser
- to turn on and off the LED on pin 9.
+  A mashup of Tom Igoe's SimpleWebServerWiFi for MKR1000 and
+  the Adafruit sensor API for the BMP100 Temperature sensor.
 
- If the IP address of your shield is yourAddress:
- http://yourAddress/H turns the LED on
- http://yourAddress/L turns it off
+  The web server lets you blink an LED via the web and publishes
+  the sensor's temperature sensor data.
 
- This example is written for a network using WPA encryption. For
- WEP or WPA, change the WiFi.begin() call accordingly.
+  This sketch will print the IP address of your WiFi Shield (once connected)
+  to the Serial monitor. From there, you can open that address in a web browser
+  to turn on and off the LED on pin 9.
 
- Circuit:
- * WiFi shield attached
- * LED attached to pin 9
+  If the IP address of your shield is yourAddress:
+  http://yourAddress/H turns the LED on
+  http://yourAddress/L turns it off
 
- created 25 Nov 2012
- by Tom Igoe
- */
+  This example is written for a network using WPA encryption. For
+  WEP or WPA, change the WiFi.begin() call accordingly.
+
+   Connections for MKR1000
+   ===========
+   Connect SCL to pin 11
+   Connect SDA to pin 11
+   Connect VDD to 3.3V DC
+   Connect GROUND to common ground
+   Wifi on board
+  LED attached to pin 9
+
+  created 24 Jan 2019
+  by Brock Craft
+
+  Code drawn from Tom Igoe, Adafruit
+*/
+
 #include <SPI.h>
 #include <WiFi101.h>
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP085_U.h>
-#include "arduino_secrets.h" 
+#include "arduino_secrets.h"
 
 ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
 char ssid[] = SECRET_SSID;        // your network SSID (name)
@@ -35,6 +47,8 @@ int keyIndex = 0;                 // your network key Index number (needed only 
 int status = WL_IDLE_STATUS;
 WiFiServer server(80);
 
+Adafruit_BMP085_Unified bmp = Adafruit_BMP085_Unified(10085);
+
 void setup() {
   Serial.begin(9600);      // initialize serial communication
   pinMode(9, OUTPUT);      // set the LED pin mode
@@ -43,6 +57,14 @@ void setup() {
   if (WiFi.status() == WL_NO_SHIELD) {
     Serial.println("WiFi shield not present");
     while (true);       // don't continue
+  }
+
+  // Initialise the sensor
+  if (!bmp.begin())
+  {
+    /* There was a problem detecting the BMP085 ... check your connections */
+    Serial.print("Ooops, no BMP085 detected ... Check your wiring or I2C ADDR!");
+    while (1);
   }
 
   // attempt to connect to WiFi network:
@@ -55,6 +77,11 @@ void setup() {
     // wait 10 seconds for connection:
     delay(10000);
   }
+  // Display some basic information on this sensor
+  displaySensorDetails();
+  Serial.print("The current temperature is: ");
+  Serial.println(getTempData());
+  // start web server
   server.begin();                           // start the web server on port 80
   printWiFiStatus();                        // you're connected now, so print out the status
 }
@@ -82,8 +109,11 @@ void loop() {
             client.println();
 
             // the content of the HTTP response follows the header:
-            client.print("Click <a href=\"/H\">here</a> turn the LED on pin 9 on<br>");
-            client.print("Click <a href=\"/L\">here</a> turn the LED on pin 9 off<br>");
+            client.print("<h3>The temperature is: ");
+            client.print(getTempData());
+            client.print(" 'C</h3>");
+            client.print("<p>Click <a href=\"/H\">here</a> turn the LED on pin 9 on<br>");
+            client.print("Click <a href=\"/L\">here</a> turn the LED on pin 9 off</p><br>");
 
             // The HTTP response ends with another blank line:
             client.println();
@@ -131,4 +161,41 @@ void printWiFiStatus() {
   // print where to go in a browser:
   Serial.print("To see this page in action, open a browser to http://");
   Serial.println(ip);
+}
+
+void displaySensorDetails(void)
+{
+  sensor_t sensor;
+  bmp.getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" hPa");
+  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" hPa");
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" hPa");
+  Serial.println("------------------------------------");
+  Serial.println("");
+  delay(500);
+}
+
+float getTempData()
+{
+  float temperature;
+  /* Get a new sensor event */
+  sensors_event_t event;
+  bmp.getEvent(&event);
+
+  /* Display the results (barometric pressure is measure in hPa) */
+  if (event.pressure)
+  {
+
+    bmp.getTemperature(&temperature);
+  }
+  else
+  {
+    Serial.println("Sensor error");
+  }
+  delay(100);
+  return temperature;
 }
